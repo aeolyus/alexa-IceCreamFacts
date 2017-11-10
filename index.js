ba4c99f3dd2bde221e49d806f910b4d490fb80c3
Richard Huang <richardhuang.huang@gmail.com>
2017-11-10 11:39:17 -0800

    fixed another typo

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var FACTS = [
     "French ice cream is enriched with egg yolks.",
     "Ice cream testers use gold spoons to be able to taste the product 100% without a slight percentage of ‘after-taste’ from typical spoons.",
     "February 4th is the National Ice Cream for Breakfast Day.",
-    "Sorbet is like ice cream but contains no milk.".
+    "Sorbet is like ice cream but contains no milk.",
     "About 9 percent of all the milk produced by U.S. dairy farmers is used to produce ice cream.",
     "Former British Prime Minister Margaret Thatcher was part of the team that first invented the method of making soft serve ice cream.",
     "In 1985, the biggest ice cream sundae was made in California. It stood twelve feet tall and was made with 4,667 gallons of ice Cream.",
